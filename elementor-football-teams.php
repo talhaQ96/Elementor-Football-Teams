3958c27bd021cbb27a71f788636bc96c7373d9e9
ct-talha-qureshi <82455739+talhaQ96@users.noreply.github.com>
2023-02-22 21:17:20 +0500

    Update elementor-football-teams.php

--- elementor-football-teams.php
+++ elementor-football-teams.php
@@ -12,11 +12,18 @@ define('PLUGIN_ROOT_PATH', plugin_dir_path( __FILE__ ));
 
 define('PLUGIN_ROOT_URL' , plugin_dir_url( __FILE__ ));
 
+
 /**
- * Include the Elementor_Football_Teams class.
+ *  Includes the plugin.php file located in the wp-admin/includes directory of a WordPress installation
+ * 
+ *  Including it here to use function `is_plugin_active()` inside file `class-elementor-football-teams.php`
+ */
+
+require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+/**
+ * Includes the Elementor_Football_Teams class.
  * 
  * The main plugin file
  */
 require PLUGIN_ROOT_PATH . 'class-elementor-football-teams.php';
-
-
